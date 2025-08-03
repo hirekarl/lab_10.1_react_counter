8c21d5ce3b1f864898932bbd31d8ab994ab91779
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-08-03 14:35:28 -0400

    Implemented increment and decrement with arrow keys; improved save to and get from localStorage.

--- react-counter/src/App.tsx
+++ react-counter/src/App.tsx
@@ -2,13 +2,15 @@ import "./App.css"
 
 import Counter from "./components/Counter/Counter"
 
-function App() {
+const App = () => {
   return (
     <>
       <div className="container-fluid">
         <div className="row">
           <div className="col-lg-4 offset-lg-4 col-md-8 offset-md-2 col-sm-10 offset-sm-1">
-            <h1 className="my-4 text-center">Lab 10.1: React Counter with useEffect</h1>
+            <h1 className="my-4 text-center">
+              Lab 10.1: React Counter with <code>useEffect</code>
+            </h1>
           </div>
           <div className="col-lg-4 offset-lg-4 col-md-8 offset-md-2 col-sm-10 offset-sm-1">
             <Counter />
--- react-counter/src/components/Counter/Counter.tsx
+++ react-counter/src/components/Counter/Counter.tsx
@@ -1,6 +1,83 @@
+import React, { useCallback, useEffect, useState } from "react"
 import { Card, Button, ButtonGroup, ListGroup } from "react-bootstrap"
+import type { status } from "../../types"
 
-export default function Counter() {
+const Counter = () => {
+  const MIN_COUNT = 0
+  const MIN_STEP_VALUE = 1
+  const LOCAL_STORAGE_DELAY = 250 // ms
+
+  const [count, setCount] = useState<number>(MIN_COUNT)
+  const [stepValue, setStepValue] = useState<number>(MIN_STEP_VALUE)
+  const [countHistory, setCountHistory] = useState<number[]>([MIN_COUNT])
+  const [status, setStatus] = useState<status>("")
+
+  const handleDecrement = useCallback((): void => {
+    setCount((prevCount) =>
+      prevCount === MIN_COUNT ? MIN_COUNT : prevCount - stepValue
+    )
+  }, [stepValue])
+
+  const handleIncrement = useCallback((): void => {
+    setCount((prevCount) => prevCount + stepValue)
+  }, [stepValue])
+
+  const handleReset = (): void => {
+    setCount(MIN_COUNT)
+    setStepValue(MIN_STEP_VALUE)
+    setCountHistory([MIN_COUNT])
+  }
+
+  const handleStepValueChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setStepValue(parseInt(event.target.value))
+  }
+
+  useEffect(() => {
+    if (countHistory[countHistory.length - 1] !== count) {
+      setCountHistory((prevCountHistory) => [...prevCountHistory, count])
+    }
+  }, [count, countHistory])
+
+  useEffect(() => {
+    setStatus("Saving to localStorage...")
+    try {
+      localStorage.setItem("counts", JSON.stringify(countHistory))
+    } catch (error) {
+      setStatus(String(error))
+    }
+  }, [countHistory])
+
+  useEffect(() => {
+    const localStorageCounts = localStorage.getItem("counts")
+    if (localStorageCounts) {
+      if (JSON.parse(localStorageCounts).length === countHistory.length) {
+        setTimeout(() => {
+          setStatus("Changes saved.")
+        }, LOCAL_STORAGE_DELAY)
+      }
+    } else {
+      setStatus("There was a problem saving to localStorage.")
+    }
+  }, [countHistory])
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent): void => {
+      if (event.key === "ArrowUp") handleIncrement()
+      else if (event.key === "ArrowDown") handleDecrement()
+    }
+    window.addEventListener("keydown", handleKeyDown)
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown)
+    }
+  }, [handleIncrement, handleDecrement])
+
+  const countHistoryList = countHistory.map((c, i) => (
+    <ListGroup.Item key={i}>{c}</ListGroup.Item>
+  ))
+
   return (
     <Card>
       <Card.Body className="text-center">
@@ -8,45 +85,49 @@ export default function Counter() {
           <h2>Counter</h2>
         </Card.Title>
         <Card.Subtitle className="my-3">
-          <h3>Current Count: 0</h3>
+          <h3>Current Count: {count}</h3>
         </Card.Subtitle>
         <ButtonGroup
           className="my-3"
           role="group"
           aria-label="Change Counter properties.">
-          <Button variant="secondary">- Decrement</Button>
-          <Button variant="primary">Increment +</Button>
-          <Button variant="danger">Reset</Button>
+          <Button
+            variant="secondary"
+            onClick={handleDecrement}
+            disabled={count === 0}>
+            - Decrement
+          </Button>
+          <Button variant="primary" onClick={handleIncrement}>
+            Increment +
+          </Button>
+          <Button variant="danger" onClick={handleReset}>
+            Reset
+          </Button>
         </ButtonGroup>
-        <Card.Text>
-          <div className="container my-3">
-            <div className="row g-3 align-items-center">
-              <div className="col-3 offset-3">
-                <label htmlFor="step-value-input" className="col-form-label">
-                  Step Value:
-                </label>
-              </div>
-              <div className="col-3">
-                <input
-                  id="step-value-input"
-                  type="number"
-                  className="form-control"
-                />
-              </div>
+        <div className="container my-3">
+          <div className="row g-3 align-items-center">
+            <div className="col-3 offset-3">
+              <label htmlFor="step-value-input" className="col-form-label">
+                Step Value:
+              </label>
+            </div>
+            <div className="col-3">
+              <input
+                id="step-value-input"
+                type="number"
+                className="form-control"
+                min={0}
+                onChange={handleStepValueChange}
+                value={stepValue}
+              />
             </div>
           </div>
-          <div className="my-3">
-            <em>Changes saved.</em>
-          </div>
-        </Card.Text>
+        </div>
+        <div className="my-3">{status}</div>
       </Card.Body>
       <Card.Footer>
         <h3 className="mb-3 text-center">Count History</h3>
-        <ListGroup className="mb-3">
-          <ListGroup.Item>1</ListGroup.Item>
-          <ListGroup.Item>2</ListGroup.Item>
-          <ListGroup.Item>3</ListGroup.Item>
-        </ListGroup>
+        <ListGroup className="mb-3">{countHistoryList}</ListGroup>
         <p className="text-muted text-center">
           Use ArrowUp to increment and ArrowDown to decrement.
         </p>
@@ -54,3 +135,5 @@ export default function Counter() {
     </Card>
   )
 }
+
+export default Counter
